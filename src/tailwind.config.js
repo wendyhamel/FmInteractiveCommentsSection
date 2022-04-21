a0f51b4b5579e67238dd2b7cf4d83a8dcdcc26fa
wendyhamel <whamel@it4web.nl>
2022-04-21 15:37:04 +0200

    Loop trough posts

---
+++ src/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Rubik', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                blue: 'hsl(238, 40%, 52%)',
+                'light-blue': 'hsl(239, 57%, 85%)',
+                'soft-red': 'hsl(358, 79%, 66%)',
+                'pale-red': 'hsl(357, 100%, 86%)',
+                'dark-blue': 'hsl(212, 24%, 26%)',
+                'gray-blue': 'hsl(211, 10%, 45%)',
+                'light-gray': 'hsl(223, 19%, 93%)',
+                'very-light-gray': 'hsl(228, 33%, 97%)',
+                white: 'hsl(0, 0%, 100%)',
+            }
+        }
+    }
+}
